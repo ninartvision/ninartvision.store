4f369492175a44bd1af01ca1d360e97850493048
Your Name <ninimzhavia@gmail.com>
2026-05-19 22:20:45 +0400

    update

--- scripts/minify-js.mjs
+++ scripts/minify-js.mjs
@@ -31,6 +31,7 @@ const PAIRS = [
   ['js/news-cards.js', 'js/news-cards.min.js'],
   ['js/room-visualizer.js', 'js/room-visualizer.min.js'],
   ['js/nv-gallery-modal.js', 'js/nv-gallery-modal.min.js'],
+  ['js/nv-cart-drawer.js', 'js/nv-cart-drawer.min.js'],
   ['sale/shop-render.js', 'sale/shop-render.min.js'],
   ['sale/shopFilter.js', 'sale/shopFilter.min.js'],
 ];
